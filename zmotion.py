db642b0d6b0540dded6255112972d098a675d612
christoph halang <christoph.halang@gmail.com>
2014-10-17 07:50:43 -0700

    detection works

--- zmotion.py
+++ zmotion.py
@@ -5,3 +5,24 @@ import time
 import datetime
 import subprocess
 from subprocess import check_output as qx
+
+# = GPIO 27
+pin_led = 13
+# = GPIO 22
+pin_detection = 15
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin_led, GPIO.OUT)
+GPIO.setup(pin_detection, GPIO.IN)
+
+
+while 1:
+	if not GPIO.input(pin_detection):
+		print chr(27) + "[2J"
+		print("Motion detected!")
+		GPIO.output(pin_led, GPIO.HIGH)
+	else:
+		print chr(27) + "[2J"
+		GPIO.output(pin_led, GPIO.LOW)
+
+GPIO.cleanup()
